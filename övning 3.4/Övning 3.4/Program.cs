1e54286baf612a9c695d94b3a7ab1cd297019177
kattborste <36436776+kattborste@users.noreply.github.com>
2018-02-26 10:36:30 +0100

    lade till övning 3.4

---
+++ övning 3.4/Övning 3.4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sida;
+
+            Console.Write("Hur lång är sidan på kuben?\t\t");
+            sida = (Convert.ToDouble(Console.ReadLine()));
+
+            Kub kuben = new Kub();
+            
+            kuben.Volym(sida);
+            kuben.Yta(sida);
+            
+            Console.ReadKey();
+
+        }
+    }
+    class Kub
+    {
+        public void Volym(double sida)
+        {
+            double volym = (Math.Pow(sida, 3));
+            Console.WriteLine("Kubens volym är " + volym);
+        }
+
+        public void Yta(double sida)
+        {
+            double yta = 6 * (Math.Pow(sida, 2));
+            Console.WriteLine("Kubens yta är " + yta);
+        }
+
+    }
+}
